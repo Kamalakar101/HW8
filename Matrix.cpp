500d8d0585bdb4f86fb92bf2ba311262b20baa40
Kamalakar Reddy <squidoodly101@gmail.com>
2025-04-17 12:03:03 -0400

    Starting on driver.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -12,12 +12,20 @@ Matrix::Matrix(int numRows, int numCols) {
     m_matrix = buildMatrix(numRows, numCols);
 }
 
+Matrix::Matrix(int numRows, int numCols, int *nums, int sizeOfNums) {
+    m_numRows = numRows;
+    m_numCols = numCols;
+    m_matrix = buildMatrix(numRows, numCols);
+    populateMatrix(nums, sizeOfNums);
+
+}
+
 Matrix::~Matrix() {
     clear();
 }
 
 void Matrix::clear() {
-    for (int i = 0; i < m_numCols; i++) {
+    for (int i = 0; i < m_numRows; i++) {
         delete m_matrix[i];
     }
     delete[] m_matrix;
@@ -35,6 +43,20 @@ int Matrix::getNumCols() {
     return m_numCols;
 }
 
+bool Matrix::populateMatrix(int *nums, int sizeOfNums) {
+    if (sizeOfNums != (m_numRows * m_numCols)) {
+        return false;
+    }
+    
+    int index = 0;
+    for (int i = 0; i < m_numRows; i++) {
+        index += m_numCols;
+        for (int j = 0; j < m_numCols; j++) {
+            m_matrix[i][j] = nums[j + index];
+        }
+    }
+}
+
 bool Matrix::addMatrix(const Matrix &rhs)
 {
     if (m_numCols != rhs.m_numCols && m_numRows != rhs.m_numRows) {
@@ -42,7 +64,7 @@ bool Matrix::addMatrix(const Matrix &rhs)
     }
 
     for (int i = 0; i < m_numRows; i++) {
-        for(int j = 0; j < m_numCols; j++) {
+        for (int j = 0; j < m_numCols; j++) {
             m_matrix[i][j] += rhs.m_matrix[i][j];
         }
     }
@@ -50,7 +72,7 @@ bool Matrix::addMatrix(const Matrix &rhs)
 
 bool Matrix::scaleMatrix(int scalar) {
     for (int i = 0; i < m_numRows; i++) {
-        for(int j = 0; j < m_numCols; j++) {
+        for (int j = 0; j < m_numCols; j++) {
             m_matrix[i][j] *= scalar;
         }
     }
@@ -62,18 +84,41 @@ bool Matrix::multiplyMatrix(const Matrix &rhs) {
     }
     int ** newMatrix = buildMatrix(m_numRows, rhs.m_numCols);
 
-    for(int i = 0; i < rhs.m_numCols; i++) {
-        for(int j = 0; j < m_numRows; j++) {
-            
+    for (int i = 0; i < m_numRows; i++) {
+    for (int j = 0; j < rhs.m_numCols; j++) {
+            for (int k = 0; k < m_numCols; k++) {
+                newMatrix[i][j] += m_matrix[i][k] * rhs.m_matrix[k][j];
+            }
         }
     }
 
+    clear();
+    m_numRows = m_numRows;
+    m_numCols = rhs.m_numCols;
+    m_matrix = newMatrix;
+    newMatrix = nullptr;
+}
+
+void Matrix::transposeMatrix() {
+    int ** newMatrix = buildMatrix(m_numCols, m_numRows);
+
+    for (int i = 0; i < m_numRows; i++) {
+        for (int j = 0; j < m_numCols; j++) {
+            newMatrix[j][i] = m_matrix[i][j];
+        }
+    }
+
+    clear();
+    m_numRows = m_numCols;
+    m_numCols = m_numRows;
+    m_matrix = newMatrix;
+    newMatrix = nullptr;
 }
 
 int ** Matrix::buildMatrix(int numRows, int numCols) {
-    int ** matrix = new int*[numCols];
-    for (int i = 0; i < m_numCols; i++) {
-        matrix[i] = new int[numRows];
+    int ** matrix = new int*[numRows];
+    for (int i = 0; i < m_numRows; i++) {
+        matrix[i] = new int[numCols]();
     }
     return matrix;
 }
--- Matrix.h
+++ Matrix.h
@@ -5,10 +5,12 @@ class Matrix{
 public:
     Matrix();
     Matrix(int numRows, int numCols);
+    Matrix(int numRows, int numCols, int * nums, int sizeOfNums);
     ~Matrix();
     void clear();
     int getNumRows();
     int getNumCols();
+    bool populateMatrix(int * nums, int sizeOfNums);
     bool addMatrix(const Matrix& rhs);
     bool scaleMatrix(int scalar);
     bool multiplyMatrix(const Matrix &rhs);
